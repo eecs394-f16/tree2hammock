caa0e89aa2d1ee998d861be0c9d766807d82ca15
Collin Pham <collinpham2018@u.northwestern.edu>
2016-09-24 11:51:50 -0500

    testing

--- src/index.js
+++ src/index.js
@@ -9,21 +9,10 @@ let app = express()
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
-mongodb.MongoClient.connect(process.env.MONGODB_URI, (err, database) => {
-  if (err) {
-    console.log(err)
-    process.exit(1)
-  }
-
-  // Save database object from the callback for reuse.
-  db = database
-  console.log("Database connection ready")
-
-  // Initialize the app.
-  app.listen(process.env.PORT || 3000, () => {
-    console.log(`Listening on port ${process.env.PORT || 3000}`)
-  })
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Listening on port ${process.env.PORT || 3000}`)
 })
+
 
 
 app.get('/', (req, res) => {
